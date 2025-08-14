4ea82a8418ce2a1507796876d6228e8f12731180
Angel-DGP <angel.patinogd@gmail.com>
2025-08-14 17:32:11 -0500

    Proyecto Invoice

---
+++ src/app/components/invoice/invoice.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from '../../services/invoice.service';
+import { Invoice } from '../../model/Invoice';
+
+@Component({
+  selector: 'app-invoice',
+  imports: [],
+  templateUrl: './invoice.component.html',
+})
+export class InvoiceComponent implements OnInit {
+  invoice!: Invoice;
+  constructor(private service: InvoiceService) {}
+  ngOnInit(): void {
+    this.invoice = this.service.getInvoice();
+  }
+}
