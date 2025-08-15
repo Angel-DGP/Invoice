b365d1e5575dc0aaf7787a919755962e19523535
Angel-DGP <angel.patinogd@gmail.com>
2025-08-14 19:27:44 -0500

    Probe

--- src/app/model/InvoiceItem.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from './Product';
-
-export class InvoiceItem {
-  id!: number;
-  product!: Product;
-  quantity!: number;
-}
--- src/model/Course.ts
+++
@@ -1,6 +0,0 @@
-import { Student } from './Student';
-export class Course {
-  id!: number;
-  name!: string;
-  students!: Student[];
-}
