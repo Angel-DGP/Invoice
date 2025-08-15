aa04af7b1f351e3c0c267a3be13330b7ea80b1e8
Angel-DGP <angel.patinogd@gmail.com>
2025-08-14 19:13:31 -0500

    Reto 9

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../model/Invoice';
+import { CompanyViewComponent } from '../company-view/company-view.component';
+import { CustomerViewComponent } from "../customer-view/customer-view.component";
+import { InvoiceDetailsViewComponent } from "../invoice-details-view/invoice-details-view.component";
 
 @Component({
   selector: 'app-invoice',
-  imports: [],
+  imports: [CompanyViewComponent, CustomerViewComponent, InvoiceDetailsViewComponent],
   templateUrl: './invoice.component.html',
 })
 export class InvoiceComponent implements OnInit {
